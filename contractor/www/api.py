f2ef717126689c7f7a1db9b856eb374390473d8a
Jenan Alfahham <jenan_fh95@hotmail.com>
2024-06-09 15:13:59 +0300

    Opportunity: Set Rates of Group Item in Group Items Table

--- contractor/www/api.py
+++ contractor/www/api.py
@@ -14,16 +14,20 @@ def set_series_number(doc):
 
     latest_group = 0
     latest_sub = 0
+    group_item = None
     for item in doc.items:
         if item.is_group:
             latest_group += 1 
             item.series_number = latest_group
             latest_sub = 0
+            group_item = item.item_code
 
         else:
+            if latest_group == 0: continue
             latest_sub += 1
             item.series_number = str(latest_group) + "_" + str(latest_sub)
 
+        item.group_item = group_item
 
 def set_rate_of_group_items(doc):
     """
@@ -32,20 +36,24 @@ def set_rate_of_group_items(doc):
     """
     rates = {}
     for item in doc.items:
-        if not item.is_group:
+        if not item.is_group and item.series_number:
             group = int(item.series_number.split('_')[0])
-            if not rates.get(group): rates[group] = 0
 
-            rates[group] += item.amount
+            if not rates.get((item.group_item, group)): 
+                rates[(item.group_item, group)] = 0
 
-    for item in doc.items:
-        if item.is_group:
-            if not rates.get(item.series_number): continue
-            item.rate = rates[item.series_number]
-            item.amount = item.rate * item.qty
+            rates[(item.group_item, group)] += item.amount
+        elif not item.series_number: 
+            item.rate = item.base_rate = item.amount = item.base_amount = 0
 
-            item.base_rate = item.rate * doc.conversion_rate
-            item.base_amount = item.amount * doc.conversion_rate
-
+    doc.group_items = []
+    for group_item, group in rates:
+        new_item = frappe._dict({
+            "item_code": group_item,
+            "item_name": frappe.db.get_value("Item", group_item, "item_name"),
+            "rate": rates[(group_item, group)],
+            "base_rate": rates[(group_item, group)] * doc.conversion_rate
+        })
+        doc.append("group_items", new_item)
 
             
