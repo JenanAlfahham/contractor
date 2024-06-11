526f4f7c8ef81687c69d26ee2a61dc91cb8a4003
Jenan Alfahham <jenan_fh95@hotmail.com>
2024-06-11 20:29:24 +0300

    Project: Set Series Number for Items Table & Calculate Amount of an Item

---
+++ contractor/public/js/doctype/project.js
@@ -0,0 +1,87 @@
+frappe.ui.form.on("Project", {
+    refresh: function(frm){
+        frm.set_query("item_code", "items", function(doc, cdt, cdn) {
+			let row = locals[cdt][cdn];
+            return {
+				filters: {
+					is_group: row.is_group
+				}
+			}
+		});
+    },
+    set_series_number: function(frm, row){
+        let latest_group = 0;
+        let latest_sub = 0;
+        let group_item = null;
+        
+        // Iterate through the items table to get the latest Group/Sub item
+        for (let i of frm.doc.items){
+            /// if we get to our index then add +1 to the previous group/sub item
+            if (i.idx == row.idx){
+                if (row.is_group) latest_group += 1;
+                
+                else {
+                    latest_sub += 1;
+                }
+                ///Set the Group Item to be the First Group Item Row
+                if (!group_item) group_item = i.item_code;
+
+                ///Set thee Group Item of the Current Item
+                i.group_item = group_item;
+                break;
+            }
+
+            else {
+                if (i.is_group){
+                    latest_group = parseInt(i.series_number)
+                    latest_sub = 0;
+                    group_item = i.item_code;
+                }
+                else if (i.series_number) {
+                    [latest_group, latest_sub] = get_group_sub(i.series_number)
+                }
+            }
+        }
+
+        if (latest_group){
+            if (row.is_group) row.series_number = latest_group;
+
+            else row.series_number = latest_group + "_" + latest_sub;
+        }
+        frm.refresh_field("items");
+    }
+})
+frappe.ui.form.on("Project Item", {
+    is_group: function(frm ,cdt, cdn){
+        let row = locals[cdt][cdn];
+        if (row.is_group && row.item_code) row.group_item = row.item_code;
+        frm.events.set_series_number(frm, row)
+    },
+    item_code: function(frm, cdt, cdn){
+        let row = locals[cdt][cdn];
+        if (row.is_group) row.group_item = row.item_code;
+    },
+    qty: function(frm, cdt, cdn){
+        let row = locals[cdt][cdn];
+        if (row.rate){
+            frappe.model.set_value(cdt, cdn, "amount", row.rate * row.qty);
+        }
+    },
+    rate: function(frm, cdt, cdn){
+        let row = locals[cdt][cdn];
+        if (row.qty){
+            frappe.model.set_value(cdt, cdn, "amount", row.rate * row.qty);
+        }
+    },
+    items_add: function(frm ,cdt, cdn){
+        console.log("$$$$")
+        frappe.model.set_value(cdt, cdn, "rate", 0.00);
+        frappe.model.set_value(cdt, cdn, "amount", 0.00);
+    }
+    
+})
+
+const get_group_sub = function(num_string){
+    let numbers = num_string.match(/\d+/g); // Matches one or more consecutive digits
+    return [parseInt(numbers[0]), parseInt(numbers[1])]
+}
