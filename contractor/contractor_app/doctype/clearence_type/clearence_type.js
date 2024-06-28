d8b81e43087555655e3cbd45d41c95b8a067d653
Jenan Alfahham <jenan_fh95@hotmail.com>
2024-06-28 21:19:57 +0300

    Add Clearence Doctype

---
+++ contractor/contractor_app/doctype/clearence_type/clearence_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Contractor and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Clearence Type', {
+	// refresh: function(frm) {
+
+	// }
+});
