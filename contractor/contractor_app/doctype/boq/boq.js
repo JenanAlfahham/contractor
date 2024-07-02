a37489ff3c50e6707832377637a6c23a8e943bc0
Jenan Alfahham <jenan_fh95@hotmail.com>
2024-07-02 22:26:03 +0300

    BOQ: Add Party Type & Fix Cost in Material Costs

--- contractor/contractor_app/doctype/boq/boq.js
+++ contractor/contractor_app/doctype/boq/boq.js
@@ -2,9 +2,48 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('BOQ', {
-	// refresh: function(frm) {
+	setup: function(frm){
+		frm.set_query("party_type", function() {
+			return{
+				"filters": {
+					"name": ["in", ["Customer", "Lead", "Prospect"]],
+				}
+			}
+		});
+	},
+	onload: function(frm){
+		frm.trigger('setup_queries');
+	},
+	party_type: function(frm) {
+		frm.trigger('setup_party_type');
 
-	// }
+		frm.set_value("party_name", "");
+	},
+	setup_party_type: function(frm) {
+		frm.trigger('setup_queries');
+		frm.trigger("set_dynamic_field_label");
+	},
+	set_dynamic_field_label: function(frm){
+		if (frm.doc.party_type) {
+			frm.set_df_property("party_name", "label", frm.doc.party_type);
+		}
+	},
+	setup_queries: function(frm){
+		if (frm.doc.party_type == "Lead") {
+			frm.set_query('party_name', erpnext.queries['lead']);
+		}
+		else if (frm.doc.party_type == "Customer") {
+			frm.set_query('party_name', erpnext.queries['customer']);
+		} else if (frm.doc.party_type == "Prospect") {
+			frm.set_query('party_name', function() {
+				return {
+					filters: {
+						"company": me.frm.doc.company
+					}
+				};
+			});
+		}
+	}
 });
 
 frappe.ui.form.on('Material costs', {
@@ -12,16 +51,16 @@ frappe.ui.form.on('Material costs', {
 		let row = locals[cdt][cdn];
 		row.qty = 1;
 		row.depreciasion_percentage = 0;
-		row.unit_cost = 0;
+		row.cost = 0;
 		frm.refresh_field("material_costs")
 	},
 	qty: function(frm, cdt, cdn){
 		let row = locals[cdt][cdn];
-		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.unit_cost);
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
 	},
-	unit_cost: function(frm, cdt, cdn){
+	cost: function(frm, cdt, cdn){
 		let row = locals[cdt][cdn];
-		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.unit_cost);
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
 	}
 });
 
