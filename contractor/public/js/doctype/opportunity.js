006be17bed9fbe7549eade2aeb2efe5c663ceea0
Jenan Alfahham <jenan_fh95@hotmail.com>
2024-07-02 22:31:58 +0300

    Opportunity: Add Group Item

--- contractor/public/js/doctype/opportunity.js
+++ contractor/public/js/doctype/opportunity.js
@@ -21,7 +21,7 @@ frappe.ui.form.on("Opportunity", {
     set_series_number: function(frm, row){
         let latest_group = 0;
         let latest_sub = 0;
-        let item_group = null;
+        let group_item = null;
         
         // Iterate through the items table to get the latest Group/Sub item
         for (let i of frm.doc.items){
@@ -33,10 +33,10 @@ frappe.ui.form.on("Opportunity", {
                     latest_sub += 1;
                 }
                 ///Set the Group Item to be the First Group Item Row
-                if (!item_group) item_group = i.item_group;
+                if (!group_item) group_item = i.item_code;
 
                 ///Set thee Group Item of the Current Item
-                i.item_group = item_group;
+                i.group_item = group_item;
                 break;
             }
 
@@ -44,7 +44,7 @@ frappe.ui.form.on("Opportunity", {
                 if (i.is_group){
                     latest_group = parseInt(i.series_number)
                     latest_sub = 0;
-                    item_group = i.item_group;
+                    group_item = i.item_code;
                 }
                 else if (i.series_number) {
                     [latest_group, latest_sub] = get_group_sub(i.series_number)
@@ -63,12 +63,12 @@ frappe.ui.form.on("Opportunity", {
 frappe.ui.form.on("Opportunity Item", {
     is_group: function(frm ,cdt, cdn){
         let row = locals[cdt][cdn];
-        if (row.is_group && row.item_code) row.item_group = row.item_group;
+        if (row.is_group && row.item_code) row.group_item = row.item_code;
         frm.events.set_series_number(frm, row)
     },
     item_code: function(frm, cdt, cdn){
         let row = locals[cdt][cdn];
-        if (row.is_group) row.item_group = row.item_group;
+        if (row.is_group) row.group_item = row.item_code;
     },
     items_add: function(frm ,cdt, cdn){
         frappe.model.set_value(cdt, cdn, "rate", 0.00);
