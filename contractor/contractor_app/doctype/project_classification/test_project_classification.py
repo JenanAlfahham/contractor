496e66307190f165ed3bc8cf79efea7761f207d6
Jenan Alfahham <jenan_fh95@hotmail.com>
2024-07-08 21:08:36 +0300

    Add Project Classification Doctype

---
+++ contractor/contractor_app/doctype/project_classification/test_project_classification.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Jenan Alfahham and contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProjectClassification(FrappeTestCase):
+	pass
