ec3084183b629ed7dafff2d999935131f11fa314
Jenan Alfahham <jenan_fh95@hotmail.com>
2024-07-08 21:08:13 +0300

    Add Engineering Consulting Office Doctype

---
+++ contractor/contractor_app/doctype/engineering_consulting_office/engineering_consulting_office.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Jenan Alfahham and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Engineering consulting office', {
+	// refresh: function(frm) {
+
+	// }
+});
